9b0c6151e2ceffa4dbc99c88e1e049566f8573d1
Brian Wolff <bawolff+wn@gmail.com>
2016-11-13 18:05:23 -0500

    Initial commit of patch from [[mw:Extension:SubpageWatchlist]]

---
+++ SubpageWatchlist.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation file for extension SubpageWatchlist
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'subpagewatchlist-desc' => 'Adds option to include subpages of watched pages in Special:Watchlist.',
+	'subpagewatchlist-rchidesubpage' => '$1 subpages',
+);
+
+$messages['qqq'] = array(
+	'subpagewatchlist-desc' => 'Description for Special:Version',
+	'subpagewatchlist-rchidesubpage' => 'Message to trigger option in Special:Watchlist',
+);
